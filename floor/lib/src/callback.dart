20fd284338b0734148bf79595853de68a2c0e981
Cassio Seffrin <cassioseffrin@gmail.com>
2022-08-15 15:05:11 -0300

    all

---
+++ floor/lib/src/callback.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:sqflite/sqflite.dart';
+
+/// Callback class that can be attached to the Floor builder.
+class Callback {
+  /// Fired when the [database] has been just created with [version].
+  final FutureOr<void> Function(
+    Database database,
+    int version,
+  ) onCreate;
+
+  /// Fired when the [database] has successfully been opened.
+  final FutureOr<void> Function(Database database) onOpen;
+
+  final FutureOr<void> Function(Database database) onConfigure;
+
+  /// Fired when the [database] has finished upgrading from [startVersion] to [endVersion].
+  final FutureOr<void> Function(
+    Database database,
+    int startVersion,
+    int endVersion,
+  ) onUpgrade;
+
+  /// Constructor.
+  const Callback(
+      {this.onConfigure, this.onCreate, this.onOpen, this.onUpgrade});
+}
